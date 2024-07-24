9295cdb01a48f81e7e87e3f3f4ef1634ceefe2fc
Arthur Knapp <knapp900@gmail.com>
2024-07-24 13:34:05 +0300

    Init commit

---
+++ src/main/java/by/ak/todo_restapi_app/TodoRestApiAppApplication.java
@@ -0,0 +1,13 @@
+package by.ak.todo_restapi_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestApiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoRestApiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/ak/todo_restapi_app/TodoRestApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ak.todo_restapi_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestApiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
